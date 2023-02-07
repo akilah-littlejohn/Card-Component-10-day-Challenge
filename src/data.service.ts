cd70ff89faee38c214a7f52b73d11b929dcf4160
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-02-06 21:30:11 -0600

    1 file modified

--- src/data.service.ts
+++ src/data.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Result } from './result';
+import { map, Observable } from 'rxjs';
 import { User } from './user';
 
 @Injectable({
@@ -10,17 +11,27 @@ export class DataService {
   urlEndpoint = 'https://randomuser.me/api/?results=1';
 
   constructor(private http: HttpClient) {}
+
+  getUser(): Observable<Result>{
+    return this.http.get<Result>(this.urlEndpoint).pipe(
+    map(this.mapResponse)
+    )
+  }
+
   private mapResponse(response: Result): Result {
-    return  {
+    return {
       info: { ...response.info },
-      results: response.results.map((user: any) => (<User>{
-        firstName: user.name.first,
-        lastName: user.name.last,
-        username: user.login.username,
-        gender: user.gender,
-        dateOfBirth: user.dob.date,
-        imageUrl: user.picture.medium
-      }))
+      results: response.results.map(
+        (user: any) =>
+          <User>{
+            firstName: user.name.first,
+            lastName: user.name.last,
+            username: user.login.username,
+            gender: user.gender,
+            dateOfBirth: user.dob.date,
+            imageUrl: user.picture.thumbnail,
+          }
+      ),
     };
   }
 }
