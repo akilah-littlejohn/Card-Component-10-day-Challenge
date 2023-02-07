f625e3283e6953b6013cc2ee382016e08b4b1842
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-02-06 21:11:33 -0600

    1 file modified

--- src/data.service.ts
+++ src/data.service.ts
@@ -3,15 +3,13 @@ import { Injectable } from '@angular/core';
 import { Result } from './result';
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root',
 })
 export class DataService {
+  urlEndpoint = 'https://randomuser.me/api/?results=1';
 
-urlEndpoint = 'https://randomuser.me/api/?results=1';
-
-  constructor(private http:HttpClient) { }
-  private mapResponse(response:Result){
-
+  constructor(private http: HttpClient) {}
+  private mapResponse(response: Result): Result {
+    return;
   }
-
 }
